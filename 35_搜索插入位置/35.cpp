42ea151b32336c60828c8caca3d4661d54ae7fd9
FeatherP <featherp@qq.com>
2025-11-01 10:35:56 +0800

    feat(搜索插入位置): 实现二分查找插入位置算法

---
+++ 35_搜索插入位置/35.cpp
@@ -0,0 +1,20 @@
+#include <vector>
+
+using namespace std;
+
+class Solution {
+public:
+    int static searchInsert(vector<int>& nums, int target) {
+        int left = 0;
+        int right = (int)nums.size() - 1;
+        while (left <= right) {
+            int mid = left + ((right - left) / 2);
+            if (nums.at(mid) < target) {
+                left = mid + 1;
+            } else {
+                right = mid - 1;
+            }
+        }
+        return left;
+    }
+};
